d1ab880eeb5c168fe6879be2ea11f04dff5a37c1
Dat <datloi95@gmail.com>
2019-10-29 01:10:05 -0400

    redux form

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
-import { Card, CardImg, CardText, CardBody,
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label,
+  Card, CardImg, CardText, CardBody,
   CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
   
   function RenderDish({dish}) {
@@ -49,30 +52,101 @@ import { Link } from 'react-router-dom';
     }
   }
 
+
+class CommentForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Rating: " + this.name.value + " Name: " + this.name.value
+        + " Comment: " + this.comment.value);
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+        <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                  <Label htmlFor="rating">Rating</Label>
+                  <Input type="number" id="rating" name="rating"
+                    min="0" max="10" innerRef={(input) => this.rating = input} />
+              </FormGroup>
+              <FormGroup>
+                  <Label htmlFor="name">Your Name</Label>
+                  <Input type="name" id="name" name="name"
+                      innerRef={(input) => this.name = input}  />
+              </FormGroup>
+              <FormGroup>
+                  <Label htmlFor="comment">Comment</Label>
+                  <textarea model=".comment" id="comment"
+                                        rows="6" className="form-control" 
+                                        innerRef={(input) => this.comment = input}/>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Submit</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    )
+  }
+
+
+}
+
+
+
 const  DishDetail = (props) => {
-  
+
   if(props.dish){
     return (
       <div className="container">
-      <div className="row">
-          <Breadcrumb>
-
-              <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-              <h3>{props.dish.name}</h3>
-              <hr />
-          </div>                
-      </div>
-      <div className="row">
-          <div className="col-12 col-md-5 m-1">
-              <RenderDish dish={props.dish} />
-          </div>
-          <div className="col-12 col-md-5 m-1">
-              <RenderComments comments={props.comments} />
-          </div>
-      </div>
+        <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
+                <CommentForm />
+            </div>
+        </div>
       </div>
   );
   } else{
